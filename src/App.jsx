bec57782edba57722b2ac2539e30d1d5763ddd03
caarlosdlg <caarllossdlg@gmail.com>
2024-03-12 21:56:03 -0600

    ya funciona el mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Button, MantineProvider, Container, Paper, Text } from '@mantine/core';
+import './App.css';
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Ositos cariñositos 2 {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <MantineProvider>
+      <Navbar/>
+      <Container padding="md" size="lg" style={{ display: 'flex' }}>
+        <div style={{ flex: 1, padding: '1rem' }}>
+          <Paper padding="md" shadow="xs">
+            <Text align="center" size="xl">Bienvenido a mi aplicación</Text>
+            <Button>Hello Mantine</Button>
+          </Paper>
+        </div>
+      </Container>
+    </MantineProvider>
     </>
   );
 }
